f27b86af1bb5d1b7b478c3f2ff9a90cf2396a130
krabobmkd <victorien.ferry@free.fr>
2025-02-14 17:08:15 +0100

    ...

---
+++ src/boopsiwizard/extracttemplate.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <zlib.h>
+
+int extractTemplate(const char *templateArchive,
+                     const char *baseName)
+{
+
+}
