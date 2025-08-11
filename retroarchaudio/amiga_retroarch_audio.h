c2e9b411b7dbe50dd4f6659495ea009e27291479
krabobmkd <victorien.ferry@free.fr>
2025-08-11 17:19:47 +0200

    ...

--- retroarchaudio/amiga_retroarch_audio.h
+++ retroarchaudio/amiga_retroarch_audio.h
@@ -1,6 +1,10 @@
 ﻿#ifndef AMIGARETROARCHAUDIO_H_
 #define AMIGARETROARCHAUDIO_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void *amiga_audio_init(const char *device,
 	unsigned rate, unsigned latency,
 	unsigned block_frames, unsigned *new_rate); // device and block_frames can be ignored, latency potentially too, new_rate should at the end contain the rate which was actually used, fixing things to 22050 also is fine
@@ -9,5 +13,9 @@ size_t amiga_audio_write(void *data, const void *s, size_t len); // Assume 16 Bi
 bool amiga_audio_stop(void *data); // Stops replay
 bool amiga_audio_start(void *data, bool is_shutdown); // starts replay
 void amiga_audio_free(void *data);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- retroarchaudio/amiga_retroarch_audio_test.c
+++ retroarchaudio/amiga_retroarch_audio_test.c
@@ -1,9 +1,92 @@
 ﻿#include "amiga_retroarch_audio.h"
 
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <exec/interupt.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+//  use a vertical blank interuption just to have some timer.
+struct Task				*myTask=NULL;
+int VBlankInterface();
+struct Interrupt VertBlank =
+{	NULL,NULL,NT_INTERRUPT,-60,"VBlank",		/* node, pri = -60 */
+	NULL,										/* data ptr, same as inputevent */
+	(void *)VBlankInterface						/* code ptr */
+};
+int vblank_ok=0;
+int VBlankInterface( void )
+{	
+	Signal(myTask,SIGBREAKF_CTRL_F);   
+	return 0; /* server chain continues		*/
+}
+
+
+void *RAAudio=NULL;
+
+static void closemain()
+{
+    if(vblank_ok) {
+     RemIntServer(INTB_VERTB, &VertBlank);
+    }	
+	printf("stop audio api\n");	
+	
+	if(RAAudio)
+	{		
+		amiga_audio_free(RAAudio);
+	}
+	
+}
 
 int main(int argc, char **argv)
 {
-
+	atexit(&closemain);
+	
+	unsigned rate=22050;
+	printf("init audio, ask freq:%d\n",rate);	
+	RAAudio = amiga_audio_init("ahi.device",
+					rate,0, // latency unused
+					0, &rate); //
+	if(!RAAudio) return 1;
+	
+	printf("init audio ok, freq is:%d\n",rate);
+	
+	printf("init some timer\n");
+	
+	myTask = FindTask(NULL);
+	AddIntServer(INTB_VERTB, &VertBlank);
+	vblank_ok = TRUE;
+	
+	printf("enter loop\n");
+	size_t subsamplelength = ((rate/50)+15) & (~15); // 16 align
+	unsigned isample=0;
+    while(1) 
+    {
+		// wait quit signal or timer
+        ULONG signals = Wait(SIGBREAKF_CTRL_C | SIGBREAKF_CTRL_F);
+		if((signals & SIGBREAKF_CTRL_C)!=0) break;
+		
+		// write some 440Hz sinus as audio
+		WORD temp[subsample*2];
+		float freqdivider = 440.0f/(rate*M_PI);
+		for(unsigned i=0;i<(subsamplelength);i++)
+		{
+			WORD s = (WORD)(sinf(((float)(i+isample))*freqdivider)*32767.0f);	
+			temp[i*2] = s
+			temp[i*2+1] = s;
+		}
+		isample += subsamplelength;
+		
+		size_t done = amiga_audio_write(RAAudio, (const void *)&temp[0],subsamplelength); 
+		
+	}
+	amiga_audio_stop(RAAudio); // Stops replay
+// is_shutdown
+	amiga_audio_start(RAAudio,TRUE); // starts replay	
+	
+	
+	// go to closemain() in all cases.
     return 0;
 }
